7aad2f86134f50f26a48b7722e2eeedd16718106
Никита Жуков <nikita.systim2@yandex.ru>
2024-04-23 20:40:13 +0800

    Добавлены файлы проекта

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const http = require('http');
-
-const hostname = 'localhost';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.end('<h1>Привет, Октагон!</h1>');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-=======
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
@@ -77,12 +61,5 @@ app.post('/updateItem', (req, res) => {
 
 // Запуск сервера
 app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
+  console.log('Сервер запущен на порту ' + port);
 });
-
-
-
-
-
-
->>>>>>> 07ccae2786adeb30ac11c90afe636c737068bb09
