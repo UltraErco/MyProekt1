0cfa31f0ab494871eea320aa62e127c41d94445b
UltraErco <167612733+UltraErco@users.noreply.github.com>
2024-05-02 19:39:48 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,88 +1,10 @@
-<<<<<<< HEAD
-const http = require('http');
+const TelegramBot = require('node-telegram-bot-api');
 
-const hostname = 'localhost';
-const port = 3000;
+const token = '6980779067:AAHBMgBXXdLy0PjIGgH2anjGZxgo4kQaldE';
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.end('<h1>Привет, Октагон!</h1>');
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Привет, октагон!');
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-=======
-const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-
-const app = express();
-const port = 3000;
-
-// Подключение к вашей базе данных MySQL
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'chatbottests',
-  password: ''
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Ошибка подключения к БД: ' + err.message);
-    return;
-  }
-  console.log('Подключение к серверу MySQL успешно установлено');
-});
-
-// Middleware для разбора тела запроса в формате JSON
-app.use(bodyParser.json());
-
-// Путь для обновления объекта в базе данных
-app.post('/updateItem', (req, res) => {
-  const { id, name, desc } = req.body;
-
-  // Проверка наличия всех необходимых параметров
-  if (!id || !name || !desc) {
-    res.send(null); // Если неправильные входные параметры, возвращаем null
-    return;
-  }
-
-  // Обновление объекта в базе данных
-  connection.query('UPDATE items SET name = ?, description = ? WHERE id = ?', [name, desc, id], (error, results) => {
-    if (error) {
-      console.error('Ошибка при обновлении объекта: ' + error.message);
-      res.send(null); // Если произошла ошибка при обновлении, возвращаем null
-      return;
-    }
-
-    // Проверяем количество обновленных строк
-    if (results.affectedRows === 0) {
-      res.json({}); // Если объект не найден, возвращаем пустой объект
-    } else {
-      // Получаем обновленный объект из базы данных
-      connection.query('SELECT * FROM items WHERE id = ?', [id], (error, updatedItem) => {
-        if (error) {
-          console.error('Ошибка при получении обновленного объекта: ' + error.message);
-          res.send(null); // Если произошла ошибка при получении, возвращаем null
-          return;
-        }
-        res.json(updatedItem[0]); // Возвращаем обновленный объект
-      });
-    }
-  });
-});
-
-// Запуск сервера
-app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
-});
-
-
-
-
-
-
->>>>>>> 07ccae2786adeb30ac11c90afe636c737068bb09
