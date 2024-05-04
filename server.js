0ea7b53ec567508cc77aa825a5ddf6a047dc07d2
UltraErco <167612733+UltraErco@users.noreply.github.com>
2024-05-04 14:56:00 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -8,3 +8,25 @@ bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Привет, октагон!');
 });
+
+bot.onText(/\/help/, (msg) => {
+    const chatId = msg.chat.id;
+    const helpMessage = `
+    Список доступных команд:
+    /site - ссылка на сайт октагона
+    /creator - информация о создателе
+    `;
+    bot.sendMessage(chatId, helpMessage);
+});
+
+bot.onText(/\/site/, (msg) => {
+    const chatId = msg.chat.id;
+    const siteUrl = 'https://students.forus.ru';
+    bot.sendMessage(chatId, `Ссылка на сайт октагона: ${siteUrl}`);
+});
+
+bot.onText(/\/creator/, (msg) => {
+    const chatId = msg.chat.id;
+    const creatorInfo = 'Жуков Никита Юрьевич';
+    bot.sendMessage(chatId, `Создатель бота: ${creatorInfo}`);
+});
